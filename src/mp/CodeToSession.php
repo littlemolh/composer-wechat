61225e22d74ff972e8c326fe0216991faf97722e
LittleMo <littlemolh@163.com>
2022-02-22 18:27:27 +0800

    使用CodeToSession接替Code2Session，当前版本暂时兼容两者

--- src/mp/CodeToSession.php
+++ src/mp/CodeToSession.php
@@ -9,12 +9,11 @@ use littlemo\wechat\Base;
 
 /**
  * TODO 小程序登录凭证校验。通过 wx.login 接口获得临时登录凭证 code 换取openid。
- * 将在下个大版本（1.3+）删除，使用CodeToSession接替
  *
  * @author sxd
  * @Date 2019-07-25 10:43
  */
-class Code2Session extends Base
+class CodeToSession extends Base
 {
 
 
@@ -29,7 +28,7 @@ class Code2Session extends Base
      * @param string $code
      * @return array
      */
-    public function Code2Openid($code, $grant_type = 'authorization_code')
+    public function CodeToOpenid($code, $grant_type = 'authorization_code')
     {
 
         $url = "https://api.weixin.qq.com/sns/jscode2session";
@@ -53,7 +52,7 @@ class Code2Session extends Base
      * @param array $config
      * @return array
      */
-    public function Code2Data($encrypted_data, $iv = '', $session_key = '')
+    public function CodeToData($encrypted_data, $iv = '', $session_key = '')
     {
         $result = [
             'code' => 200,
